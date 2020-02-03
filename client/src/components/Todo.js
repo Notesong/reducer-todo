c7f721751d15e37e80ab13e1e23d91e8b3d8a33c
Lee Damien <notesong.git@outlook.com>
2020-02-02 19:50:00 -0500

    Class name correction

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const Todo = ({ item, dispatch }) => {
     return (
-        <div className="todo">
-            <div 
-                // let user declare task completed by clicking it
-                onClick={e => dispatch({ type: 'TOGGLE_COMPLETED', payload: item.id}) }
-                // changes css based on whether task completed
-                className={`task${item.completed ? " completed" : ""}`}
-            >
-                <p>{item.task}</p>
-            </div>
+        <div 
+            // let user declare task completed by clicking it
+            onClick={e => dispatch({ type: 'TOGGLE_COMPLETED', payload: item.id}) }
+            // changes css based on whether task completed
+            className={`todo${item.completed ? " completed" : ""}`}
+        >
+            <p>{item.task}</p>
 
         </div>
     )
